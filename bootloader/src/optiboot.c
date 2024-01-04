66fe1aa3f029e90394315c93fec9ddf1237ffcaa
Filip Parag <filip@parag.rs>
2024-01-04 22:54:49 +0100

    verbose bootloader

--- bootloader/src/optiboot.c
+++ bootloader/src/optiboot.c
@@ -21,9 +21,39 @@ void put_dbg(const char *const buffer) {
     __uart1_tx = *character;
     ++character;
   }
-  while (!__uart1_tx_ready)
-    ;
-  __uart1_tx = '\n';
+}
+
+void put_num(const usize number, const usize base) {
+  char buffer[32];
+  usize n = number;
+  usize d = 0;
+  do {
+    char a = n % base;
+    buffer[d] = (a < 10 ? '0' + a : 'A' + a - 10);
+    n /= base;
+    ++d;
+  } while (n != 0);
+  switch (base) {
+  case 2:
+    while (d < 8) {
+      buffer[d] = '0';
+      ++d;
+    }
+    break;
+  case 16:
+    while (d < 2) {
+      buffer[d] = '0';
+      ++d;
+    }
+    break;
+  }
+  for (usize i = 0; i < d / 2; ++i) {
+    char a = buffer[i];
+    buffer[i] = buffer[d - i - 1];
+    buffer[d - i - 1] = a;
+  }
+  buffer[d] = '\0';
+  put_dbg(buffer);
 }
 
 void sleep(const u64 interval_ms) {
@@ -92,11 +122,20 @@ void stk_univeral(void) {
 void stk_prog_page(const u16 address) {
   u8 length = get_length();
   get_ch(); // desttype
-  volatile usize *const bram = (usize *)&__fw_start;
+  volatile char *const bram = (char *)&__fw_start;
   const usize page_start = address << 1;
+  put_dbg("Page start: ");
+  put_num(page_start, 16);
+  put_dbg("\n");
   for (usize byte = 0; byte < length; ++byte) {
     const usize offset = page_start + byte;
     *(bram + offset) = get_ch();
+    put_num(*(bram + offset), 16);
+    if (byte > 0 && byte % 4 == 3) {
+      put_dbg("\n");
+    } else {
+      put_dbg(" ");
+    }
   }
   verify_space();
 }
@@ -105,7 +144,7 @@ void stk_read_page(const u16 address) {
   u8 length = get_length();
   get_ch(); // desttype
   verify_space();
-  const volatile usize *const bram = (usize *)&__fw_start;
+  const volatile char *const bram = (char *)&__fw_start;
   const usize page_start = address << 1;
   for (usize byte = 0; byte < length; ++byte) {
     const usize offset = page_start + byte;
@@ -122,50 +161,58 @@ void stk_read_sign(void) {
 
 void optiboot(void) {
   flash_led();
-  put_dbg("Optiboot started!");
+  put_dbg("\n\nOptiboot started after ");
+  put_num((usize)__counter_millis, 10);
+  put_dbg(" ms.\n");
   u16 address;
   for (;;) {
     switch (get_ch()) {
     case STK_GET_PARAMETER:
       stk_get_parameter();
-      put_dbg("STK_GET_PARAMETER");
+      put_dbg("STK_GET_PARAMETER\n");
       break;
     case STK_SET_DEVICE:
       get_n_ch(20);
-      put_dbg("STK_SET_DEVICE");
+      put_dbg("STK_SET_DEVICE\n");
       break;
     case STK_SET_DEVICE_EXT:
       get_n_ch(5);
-      put_dbg("STK_SET_DEVICE_EXT");
+      put_dbg("STK_SET_DEVICE_EXT\n");
       break;
     case STK_LEAVE_PROGMODE:
       verify_space();
       put_ch(STK_OK);
-      put_dbg("STK_LEAVE_PROGMODE");
+      put_dbg("STK_LEAVE_PROGMODE\n");
       return;
     case STK_LOAD_ADDRESS:
       stk_load_address(&address);
-      put_dbg("STK_LOAD_ADDRESS");
+      put_dbg("STK_LOAD_ADDRESS ");
+      put_num(address, 16);
+      put_dbg("\n");
       break;
     case STK_UNIVERSAL:
       stk_univeral();
-      put_dbg("STK_UNIVERSAL");
+      put_dbg("STK_UNIVERSAL\n");
       break;
     case STK_PROG_PAGE:
       stk_prog_page(address);
-      put_dbg("STK_PROG_PAGE");
+      put_dbg("STK_PROG_PAGE ");
+      put_num(address, 16);
+      put_dbg("\n");
       break;
     case STK_READ_PAGE:
       stk_read_page(address);
-      put_dbg("STK_READ_PAGE");
+      put_dbg("STK_READ_PAGE ");
+      put_num(address, 16);
+      put_dbg("\n");
       break;
     case STK_READ_SIGN:
       stk_read_sign();
-      put_dbg("STK_READ_SIGN");
+      put_dbg("STK_READ_SIGN\n");
       break;
     default:
       verify_space();
-      put_dbg("STK_DEFAULT");
+      put_dbg("STK_DEFAULT\n");
     }
     put_ch(STK_OK);
   }
